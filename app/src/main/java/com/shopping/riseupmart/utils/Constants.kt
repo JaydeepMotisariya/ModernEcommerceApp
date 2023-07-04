ea654fd3caf863526edce9f0e4e64925e88646b8
Jaydeep Motisariya <jmotisariya@gmail.com>
2023-07-04 15:12:22 -0400

    Save the user data in Firestore Database

---
+++ app/src/main/java/com/shopping/riseupmart/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.shopping.riseupmart.utils
+
+object Constants {
+    const val USER_COLLECTION = "user"
+}
--- app/src/main/java/com/shopping/riseupmart/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/shopping/riseupmart/viewmodel/RegisterViewModel.kt
@@ -3,7 +3,9 @@ package com.shopping.riseupmart.viewmodel
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.shopping.riseupmart.data.User
+import com.shopping.riseupmart.utils.Constants.USER_COLLECTION
 import com.shopping.riseupmart.utils.RegisterFieldsState
 import com.shopping.riseupmart.utils.RegisterValidation
 import com.shopping.riseupmart.utils.Resource
@@ -19,11 +21,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor(
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val db: FirebaseFirestore
 ) : ViewModel() {
 
-    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Unspecified())
-    val register: Flow<Resource<FirebaseUser>> = _register
+    private val _register = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val register: Flow<Resource<User>> = _register
 
     private val _validation = Channel<RegisterFieldsState>()
     val validation = _validation.receiveAsFlow()
@@ -36,7 +39,8 @@ class RegisterViewModel @Inject constructor(
             firebaseAuth.createUserWithEmailAndPassword(user.email, password)
                 .addOnSuccessListener {
                     it.user?.let {
-                        _register.value = Resource.Success(it)
+
+                        saveUserInfo(it.uid, user)
                     }
                 }.addOnFailureListener {
                     _register.value = Resource.Error(it.message.toString())
@@ -49,6 +53,18 @@ class RegisterViewModel @Inject constructor(
                 _validation.send(registerFieldsState)
             }
         }
+    }
+
+    private fun saveUserInfo(userUid: String, user: User) {
+        db.collection(USER_COLLECTION)
+            .document(userUid)
+            .set(user)
+            .addOnSuccessListener {
+                _register.value = Resource.Success(user)
+            }
+            .addOnFailureListener {
+                _register.value = Resource.Error(it.message.toString())
+            }
     }
 
     private fun checkValidation(user: User, password: String): Boolean {
