98cbc135f257429fb2e4e86a505b9110227b07d1
Jaydeep Motisariya <jmotisariya@gmail.com>
2023-07-11 10:25:19 -0400

    Implemented all categories fragments

---
+++ app/src/main/java/com/shopping/riseupmart/viewmodel/factory/BaseCategoryViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.shopping.riseupmart.viewmodel.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import com.shopping.riseupmart.data.Category
+import com.shopping.riseupmart.viewmodel.CategoryViewModel
+
+class BaseCategoryViewModelFactoryFactory(
+    private val firestore: FirebaseFirestore,
+    private val category: Category
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return CategoryViewModel(firestore,category) as T
+    }
+}
